657705a10b0a2970c5aad2681e712db9e16ec995
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-02-23 09:55:05 -0400

    Adicionado Copyright
    Adicionado os criadores da pagina

--- JS/index.js
+++ JS/index.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const imagens = document.querySelectorAll('.imagens-section .background-image');
     const setaEsquerda = document.querySelector('.seta-esquerda');
     const setaDireita = document.querySelector('.seta-direita');
-    const infoBox = document.querySelectorAll('.info-box');
-    const indicadoresLista = document.getElementById('indicadores-lista');
 
     let imagemAtualIndex = 0;
 
@@ -40,7 +38,6 @@ document.addEventListener('DOMContentLoaded', function () {
         mostrarProximaImagem();
     }, 15000);
 
-    // Adiciona indicadores
     imagens.forEach((_, index) => {
         const indicador = document.createElement('li');
         indicador.classList.add('indicador');
@@ -75,13 +72,6 @@ document.addEventListener('DOMContentLoaded', function () {
         imagens.forEach((imagem, index) => {
             if (index === imagemAtualIndex) {
                 imagem.style.display = 'block';
-
-//                setTimeout(() => {
- //                   infoBox[index].style.opacity = '1';
- //               }, 500);
- //           } else {
-//                imagem.style.display = 'none';
- //               infoBox[index].style.opacity = '0';
             }
         });
 
@@ -90,42 +80,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function ocultarImagemAtual() {
         imagens[imagemAtualIndex].style.display = 'none';
-    }
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    const itemsComSubmenu = document.querySelectorAll('.has-submenu');
-    const indicadoresLista = document.getElementById('indicadores-lista');
-
-    itemsComSubmenu.forEach((item, index) => {
-        const submenu = item.querySelector('.submenu');
-
-        item.addEventListener('click', () => {
-            closeAllSubmenus();
-            submenu.classList.toggle('show-submenu');
-            updateIndicadores(index);
-        });
-    });
-
-    function closeAllSubmenus() {
-        itemsComSubmenu.forEach(item => {
-            const submenu = item.querySelector('.submenu');
-            submenu.classList.remove('show-submenu');
-        });
-    }
-
-    function updateIndicadores(index) {
-        // Limpa os indicadores existentes
-        indicadoresLista.innerHTML = '';
-
-        // Adiciona indicadores para cada imagem
-        for (let i = 0; i < itemsComSubmenu.length; i++) {
-            const indicador = document.createElement('li');
-            indicador.classList.add('indicador');
-            if (i === index) {
-                indicador.classList.add('ativo');
-            }
-            indicadoresLista.appendChild(indicador);
-        }
     }
 });
