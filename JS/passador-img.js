5e5fe0948ef8fc5efa3dae4ad4cf58eb56f60866
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-03-12 17:23:35 -0400

    Troca do JS
    Feito a troca e arrumação do CSS agora funcionando da forma correta e trocando as imagens de forma certa como se fosse um carrosel e nao mais travando ou deixando a tela maior

--- JS/passador-img.js
+++ JS/passador-img.js
@@ -1,36 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const imagensSection = document.querySelector('.imagens-section');
-    const blocosImagem = document.querySelectorAll('.bloco-imagem');
+    const secoes = document.querySelectorAll('.bloco-informativo');
     const setaEsquerda = document.getElementById('seta-esquerda');
     const setaDireita = document.getElementById('seta-direita');
 
     let indiceAtual = 0;
-    const numImagensVisiveis = 3;
+    const numSecoesVisiveis = 3;
 
     setaDireita.addEventListener('click', function () {
-        if (indiceAtual < blocosImagem.length - numImagensVisiveis) {
+        if (indiceAtual < secoes.length - numSecoesVisiveis) {
             indiceAtual++;
-            atualizarVisibilidadeImagens();
+            atualizarVisibilidadeSecoes();
         }
     });
 
     setaEsquerda.addEventListener('click', function () {
         if (indiceAtual > 0) {
             indiceAtual--;
-            atualizarVisibilidadeImagens();
+            atualizarVisibilidadeSecoes();
         }
     });
 
-    function atualizarVisibilidadeImagens() {
-        blocosImagem.forEach(function (bloco, indice) {
-            if (indice >= indiceAtual && indice < indiceAtual + numImagensVisiveis) {
-                bloco.style.display = 'block';
+    function atualizarVisibilidadeSecoes() {
+        secoes.forEach(function (secao, indice) {
+            if (indice >= indiceAtual && indice < indiceAtual + numSecoesVisiveis) {
+                secao.style.display = 'block';
             } else {
-                bloco.style.display = 'none';
+                secao.style.display = 'none';
             }
         });
     }
 
     // Inicializar visibilidade
-    atualizarVisibilidadeImagens();
+    atualizarVisibilidadeSecoes();
 });
