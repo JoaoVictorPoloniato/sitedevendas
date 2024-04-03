d98a7c5cbdb41d19a1e38c81d759003b432c39a6
Weverton Pereira Andrade <weverton.anvy@gmail.com>
2024-04-03 16:55:19 -0400

    Ajuste da página inicial.
    Foi realizado a estilização e ajuste de todo o cabeçalho e o posicionamento do iframe que estava sendo cortado uma parte inferior.

--- JS/toggle.js
+++ JS/toggle.js
@@ -1,20 +1,17 @@
 
 function toggleSubMenu(index) {
-    var subMenus = document.querySelectorAll('.main-menu ul ul');
+    var subMenus = document.querySelectorAll('.menu-segundario');
     subMenus.forEach(function (subMenu, i) {
         if (i !== index) {
             subMenu.style.display = 'none';
         }
     });
-
     subMenus[index].style.display = subMenus[index].style.display === 'none' ? 'block' : 'none';
 }
-
 function hideSubMenu(index) {
-    var subMenus = document.querySelectorAll('.main-menu ul ul');
+    var subMenus = document.querySelectorAll('.menu-segundario');
     subMenus[index].style.display = 'none';
 }
-
 window.addEventListener('load', function () {
     hideSubMenu(0);
 });
