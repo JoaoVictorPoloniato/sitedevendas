b259a6a114d2653f2661ebe1bff6abefb5f14d53
Weverton Pereira Andrade <weverton.anvy@gmail.com>
2024-04-04 15:10:35 -0400

    Voltando Versão
    Por conta de Bug

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     const imagensSection = document.getElementById('imagens-section');
     const imagens = document.querySelectorAll('.imagens-section .background-image');
     const setaEsquerda = document.querySelector('.seta-esquerda');
     const setaDireita = document.querySelector('.seta-direita');
 
     let imagemAtualIndex = 0;
-    let temporizadorTrocaAutomatica;
+    let temporizadorTrocaAutomatica; // Declaramos a variável para armazenar o temporizador
 
     function iniciarTemporizador() {
         temporizadorTrocaAutomatica = setInterval(() => {
             mostrarProximaImagem();
-        }, 5000);
+        }, 10000);
     }
 
     function pararTemporizador() {
@@ -23,18 +22,18 @@ document.addEventListener('DOMContentLoaded', function () {
         iniciarTemporizador();
     }
 
-    iniciarTemporizador(); 
+    iniciarTemporizador(); // Iniciar o temporizador quando o script for carregado
 
     imagensSection.addEventListener('mouseenter', () => {
         setaEsquerda.style.opacity = '1';
         setaDireita.style.opacity = '1';
-        pararTemporizador(); 
+        pararTemporizador(); // Quando o mouse entra na área das imagens, paramos o temporizador
     });
 
     imagensSection.addEventListener('mouseleave', () => {
         setaEsquerda.style.opacity = '0';
         setaDireita.style.opacity = '0';
-        iniciarTemporizador(); 
+        iniciarTemporizador(); // Quando o mouse sai da área das imagens, iniciamos o temporizador novamente
     });
 
     setaEsquerda.addEventListener('mouseenter', () => {
@@ -48,44 +47,31 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     setaEsquerda.addEventListener('click', () => {
-        pararTemporizador(); 
+        pararTemporizador(); // Quando a seta esquerda é clicada, paramos o temporizador
         mostrarImagemAnterior();
-        reiniciarTemporizador(); 
+        reiniciarTemporizador(); // Reiniciamos o temporizador após a troca de imagem
     });
 
     setaDireita.addEventListener('click', () => {
-        pararTemporizador();
+        pararTemporizador(); // Quando a seta direita é clicada, paramos o temporizador
         mostrarProximaImagem();
-        reiniciarTemporizador();
+        reiniciarTemporizador(); // Reiniciamos o temporizador após a troca de imagem
     });
 
-    const indicadoresLista = document.getElementById('indicadores-lista');
+    const indicadoresLista = document.getElementById('indicadores-lista'); // Adicionando a seleção do elemento para adicionar os indicadores
 
-    const primeiroIndicador = document.createElement('li');
-    primeiroIndicador.classList.add('indicador', 'ativo');
-    indicadoresLista.appendChild(primeiroIndicador);
-    
-    primeiroIndicador.addEventListener('click', () => {
-        pararTemporizador();
-        mostrarImagem(0); 
-        reiniciarTemporizador();
-    });
-    
     imagens.forEach((_, index) => {
-        if (index !== 0) {
-            const indicador = document.createElement('li');
-            indicador.classList.add('indicador');
-            indicadoresLista.appendChild(indicador);
-    
-            indicador.addEventListener('click', () => {
-                pararTemporizador();
-                mostrarImagem(index); 
-                reiniciarTemporizador();
-            });
-        }
+        const indicador = document.createElement('li');
+        indicador.classList.add('indicador');
+        indicadoresLista.appendChild(indicador);
+
+        indicador.addEventListener('click', () => {
+            pararTemporizador(); // Quando um indicador é clicado, paramos o temporizador
+            mostrarImagem(index); // Quando clicado em um indicador, mostra a imagem correspondente
+            reiniciarTemporizador(); // Reiniciamos o temporizador após a troca de imagem
+        });
     });
-    
-    
+
     function mostrarImagem(index) {
         ocultarImagemAtual();
         imagemAtualIndex = index;
@@ -105,23 +91,22 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function mostrarImagemAtual() {
-        imagens[imagemAtualIndex].style.display = 'block';
+        imagens[imagemAtualIndex].style.display = 'block'; // Mostra a imagem atual
         atualizarIndicadores();
     }
 
     function ocultarImagemAtual() {
-        imagens[imagemAtualIndex].style.display = 'none';
+        imagens[imagemAtualIndex].style.display = 'none'; // Oculta a imagem atual
     }
 
     function atualizarIndicadores() {
         const indicadores = document.querySelectorAll('.indicadores .indicador');
         indicadores.forEach((indicador, index) => {
             if (index === imagemAtualIndex) {
-                indicador.classList.add('ativo'); 
+                indicador.classList.add('ativo'); // Adiciona a classe 'ativo' para indicar que está selecionado
             } else {
-                indicador.classList.remove('ativo'); 
+                indicador.classList.remove('ativo'); // Remove a classe 'ativo' dos outros indicadores
             }
         });
     }
 });
-
